247c931c931d73ae4c821c653832c7270d2de571
Sebastian Wiercinski <seb@stianw.de>
2015-11-16 18:34:20 +0100

    Implement Bootstrap UI + Add Retina Sizes incl css3 scaling

--- js/settings.js
+++ js/settings.js
@@ -2,18 +2,21 @@ window.SETTINGS = {
 	'embedlyKey': 'dev', // insert your Embedly API key here
 	'options': {
 		'platform': {
-			'default': 'facebook',
+			'default': 'Facebook Retina',
 			'choices': {
-				'facebook': { w: 1200, h: 627 },
-				'twitter': { w: 1024, h: 512 },
-				'instagram': {w: 640, h: 640 }
+				'Facebook Retina': { w: 2400, h: 1254 },
+				'Twitter Retina': { w: 2048, h: 1024 },
+				'Instagram Retina': {w: 1280, h: 1280 },
+                'Facebook': { w: 1200, h: 627 },
+				'Twitter': { w: 1024, h: 512 },
+				'Instagram': {w: 640, h: 640 }
 			}	
 		},
 		'font': {
 			'default': 'helvetica',
 			'choices': {
 				'helvetica': 'Helvetica, Arial, sans-serif',
-				'georgia': 'Georgia, Times New Roman, serif'
+				'georgia': 'Georgia, Times New Roman, serif',
 			}
 		}
 	},
